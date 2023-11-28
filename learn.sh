c8364877aedbde0e245a2a47546dce908307e158
hardik1408 <hardikj22@iitk.ac.in>
2023-11-29 03:49:33 +0530

    "functions"

--- learn.sh
+++ learn.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-read -p "what is your name" name
+function hello(){
+	echo "hello world"
+}
 
-while [[ -z ${name} ]]
-do
-	echo "name cannot be blank. try again"
-	read -p "enter name again" name
-done
-
-echo "your name is ${name}"
+hello
