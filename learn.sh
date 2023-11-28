c08c083202a21443e836334fabcf1b0e18b8ab70
hardik1408 <hardikj22@iitk.ac.in>
2023-11-29 03:46:53 +0530

    "while_loop"

--- learn.sh
+++ learn.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-ctr=1
-while [[ $ctr -le 10 ]]
+read -p "what is your name" name
+
+while [[ -z ${name} ]]
 do
-	echo "${ctr}"
-	((ctr++))
+	echo "name cannot be blank. try again"
+	read -p "enter name again" name
 done
+
+echo "your name is ${name}"
