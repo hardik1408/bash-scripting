f12bd8129c15bf47ffa7b4ac247894a8e99a51f7
hardik1408 <hardikj22@iitk.ac.in>
2023-12-01 01:49:14 +0530

    "server"

--- server.sh
+++ server.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+\#!/bin/bash
 
-##
+############################################
 # bash script that checks:
 #	-memory usage
 #	- cpu load
 #	- number of tcp connections
 #	- kernel version
-##
+############################################
 
 server_name=$(hostname)
 
